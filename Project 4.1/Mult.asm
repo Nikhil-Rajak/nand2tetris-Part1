9f399a8a64611adda757d7ac616c000c1cdbee0b
Nikhil Rajak <82169462+Nikhil-Rajak@users.noreply.github.com>
2025-03-03 18:18:36 +0530

    Add files via upload

--- Project 4.1/Mult.asm
+++ Project 4.1/Mult.asm
@@ -1,24 +1,40 @@
-@sum
-M=0   //sum=0
-@i
-M=1   //i=1
-(LOOP)
-@i    
-D=M   //D=i
-@R0
-D=D-M //D=i-R0
-@END
-D;JGT  // if(i-R0) > 0 goto END
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+
+// Put your code here.
+
+(START)
 @R1
-D=M   //D=R1
-@sum
-M=D+M  //sum+=R1
-@i
-M=M+1  //i=i+1
-@LOOP
-0;JMP  //Goto LOOP
-(END)
-@sum
 D=M
-@R2
+@i
 M=D
+@R2
+M=0
+
+(LOOP)
+    @i
+    D=M
+    @START
+    D;JEQ
+    @R0
+    D=M
+    @R2
+    M=M+D
+    @i
+    M=M-1
+    D=M
+    @END
+    D;JEQ
+
+@LOOP
+0;JMP
+
+
+(END)
+    @END
+    0;JMP
